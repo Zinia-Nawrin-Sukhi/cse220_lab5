b060817da0ee61578d795f3e6657248f8d3363b2
Zinia-Nawrin-Sukhi <83065711+Zinia-Nawrin-Sukhi@users.noreply.github.com>
2021-04-23 16:47:44 +0600

    Add files via upload

---
+++ DoublyList.java
@@ -0,0 +1,145 @@
+public class DoublyList{
+    public Node head;
+    
+    /* First Constructor:
+     * Creates a linked list using the values from the given array. head will refer
+     * to the Node that contains the element from a[0]
+     */ 
+    public DoublyList(Object [] a){
+        head = new Node(null, null, null);
+        Node tail = head;
+        
+        for(int i = 0; i<a.length; i++){
+            Node mn = new Node(a[i], null, null);
+            tail.next = mn;
+            mn.prev=tail;
+            tail=tail.next;
+        }
+        tail.next=head; // Making it circular
+        head.prev=tail;
+    }
+    
+    /* Second Constructor:
+     * Sets the value of head. head will refer
+     * to the given LinkedList
+     */
+    public DoublyList(Node h){
+        head = h;
+    }
+    
+    /* Counts the number of Nodes in the list */
+    public int countNode(){
+        int count=0;
+        for(Node n=head.next; n!=head; n=n.next){
+          count++;
+        }
+        return count; 
+    }
+    
+    /* prints the elements in the list */
+    public void forwardprint(){
+      for(Node n=head.next; n!=head; n=n.next){
+        if(n.next==head){
+          System.out.print(n.element+".");
+        }else{
+          System.out.print(n.element+", ");
+        }
+      }
+      System.out.println();
+    }
+    
+    public void backwardprint(){
+        for(Node n=head.prev; n!=head; n=n.prev){
+        if(n.prev==head){
+          System.out.print(n.element+".");
+        }else{
+          System.out.print(n.element+", ");
+        }
+      }
+      System.out.println();
+    }
+    
+    
+    // returns the reference of the Node at the given index. For invalid index return null.
+    public Node nodeAt(int idx){
+        int count=0;
+        Node x=null;
+        for(Node n=head.next; n!=head; n=n.next){
+          if(count==idx){
+            x=n;
+          }
+          count++;
+        }
+        
+        return x; 
+    }
+    
+    
+    
+    /* returns the index of the Node containing the given element.
+     if the element does not exist in the List, return -1.
+     */
+    public int indexOf(Object elem){
+      int index=0;
+      boolean boo=false;
+       for(Node n=head.next; n!=head; n=n.next){
+        if(n.element==elem){
+          boo=true;
+          break;
+       }
+       index++;
+      }
+    if(boo==true){
+      return index; 
+     }else{
+      return -1;
+     }
+  }
+    
+    
+    /* inserts Node containing the given element at the given index
+     * Check validity of index.
+     */
+    public void insert(Object elem, int idx){
+        Node n=new Node (elem, null, null);
+        Node p=null; //the node after which the new node is to be connected
+        if(idx==0){
+          p=head;
+        }else{
+          p=nodeAt(idx-1);
+        }
+        Node q=p.next; //the node before which the new node is to be connected
+        
+        n.prev=p;
+        n.next=q;
+        q.prev=n;
+        p.next=n;
+    }
+    
+    
+    /* removes Node at the given index. returns element of the removed node.
+     * Check validity of index. return null if index is invalid.
+     */
+    public Object remove(int index){
+        int count=countNode();
+        if(index>=0||index<count){
+          Node n= nodeAt(index);
+          Node p= n.prev;
+          Node q= n.next;
+          p.next=q;
+          q.prev=p;
+          
+          Object x=n.element;
+          n.element=null;
+          n.next=null;
+          n.prev=null;
+          n=null;
+          return x;
+        }else{
+          return null;
+        }
+    }
+    
+    
+    
+}
